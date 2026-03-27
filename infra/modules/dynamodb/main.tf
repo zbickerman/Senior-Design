2d3f65da943caeac6e3abbc339c7370d5cf85bbd
fperaza <Frank.E.Peraza@outlook.com>
2026-03-26 22:54:35 -0400

    Fix Terraform formatting

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,16 +1,16 @@
 resource "aws_dynamodb_table" "work_orders" {
-    name = var.table_name
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "work_order_id"
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "work_order_id"
 
 
-    attribute {
-      name = "work_order_id"
-      type = "S"
-    }
+  attribute {
+    name = "work_order_id"
+    type = "S"
+  }
 
 
-    tags = merge(var.tags, {
-      ManagedBy = "Terraform"
-    })
+  tags = merge(var.tags, {
+    ManagedBy = "Terraform"
+  })
 }
