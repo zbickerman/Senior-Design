32c51f5996d60e236ae07e8186fd01e2707f383e
fperaza <Frank.E.Peraza@outlook.com>
2026-04-02 20:43:01 -0400

    Added lambda semantic structure and IAM policies

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -7,3 +7,78 @@ module "dynamodb" {
     Owner       = "Frank"
   }
 }
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+
+}
+
+module "lambda_role" {
+  source = "../../modules/iam-role"
+
+  name               = "workorder-processor-role-dev"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+
+  policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+# module "lambda_function" {
+#   source = "../../modules/lambda"
+
+#   function_name = "workorder-processor-dev"
+#   role_arn      = module.lambda_role.arn
+
+#   handler  = "lambda_function.lambda_handler"
+#   runtime  = "java25"
+#   filename = "${path.root}/../../lambda/workorder_processor.zip"
+# }
+
+data "aws_iam_policy_document" "developer_passrole_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:PassRole"
+    ]
+
+    resources = [
+      module.lambda_role.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "lambda:CreateFunction",
+      "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
+      "lambda:GetFunction",
+      "lambda:ListFunctions"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "developer_lambda_policy" {
+  name   = "developer-lambda-passrole-dev"
+  policy = data.aws_iam_policy_document.developer_passrole_policy.json
+}
+
+
+resource "aws_iam_group_policy_attachment" "attach_dev_group_policy" {
+  group      = "DevGroup"
+  policy_arn = aws_iam_policy.developer_lambda_policy.arn
+}
